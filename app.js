6a94b8dfdca1098fbf2bd88dc58f0689fbe6f972
Seong-uk-An <asu970602@naver.com>
2021-09-02 12:24:04 +0900

    length

--- app.js
+++ app.js
@@ -1,32 +1,7 @@
-const sentences = [
-  "      ABC   abd",
-  "ABC  abc        ",
-  `       first
-second third      
-           forth
-           
-sentence
+const arr = ["short", "long sentence, it is not appropriate"];
 
-
-
-
-
-
-      
-    
-      `,
-];
-
-const filterSentence = (sentences) => {
-  const filtered = [];
-  sentences.forEach((s) => filtered.push(s.trim()));
-  return filtered;
-};
-
-const filter = filterSentence(sentences);
-console.log(filter);
-console.log(typeof filter);
-
-const trimedSentence = sentences.map((sentence) => sentence.trim());
-console.log(trimedSentence);
-console.log(typeof trimedSentence);
+arr.map((s) => {
+  if (s.length < 10) {
+    console.log(s);
+  }
+});
