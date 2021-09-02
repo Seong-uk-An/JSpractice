bd60f5c5b9ade04e6d5d3793b6e0f895d9b0af57
Seong-uk-An <asu970602@naver.com>
2021-09-02 16:55:15 +0900

    reduce

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-const arr = [
-  { id: 0, name: "혜림", age: 6 },
-  { id: 1, name: "현일", age: 3 },
-  { id: 2, name: "현아", age: 5 },
-  { id: 3, name: "우림", age: 2 },
-];
+const numArr = [1, 2, 3, 4, 5];
 
-const arr2 = arr.map((el) => {
-  el.age++;
-  return el;
-});
-
-const arr3 = arr.map((el) => el.name);
-
-console.log(arr2);
-console.log(arr3);
+const result = numArr.reduce((acc, el) => {
+  return acc + el;
+}, 0);
