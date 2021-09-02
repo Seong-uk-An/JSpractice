5f80ea105ac3e8c0f29d726cfe2a0995a26ca7b8
Seong-uk-An <asu970602@naver.com>
2021-09-02 17:19:16 +0900

    freeze

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const obj = {
-  movie: "Sunny",
-  music: "Like Sugar",
-  style: "Retro",
-  price: Infinity,
+let obj = {};
+
+obj.title = "IDOL";
+obj = Object.freeze(obj);
+// obj.title = "Euphoria";
+
+console.log(obj);
+
+const changeUntilNum = (obj, num) => {
+  "use strict";
+
+  while (true) {
+    console.log(obj);
+
+    if (obj.age >= num) {
+      obj = Object.freeze(obj);
+    }
+    obj.age += 1;
+  }
 };
 
-const arr = Object.keys(obj);
-
-console.log(arr);
+let profile = { name: "지연", age: 25 };
+changeUntilNum(profile, 30);
