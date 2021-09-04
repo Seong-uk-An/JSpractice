4e1189243126fa66d64364e5a5ad9bc08c5361fb
Seong-uk-An <asu970602@naver.com>
2021-09-04 13:51:41 +0900

    replace

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const str = "Java is not in Javascript";
+console.log("2018-08-03 07-23-14".replace("-", ":"));
+console.log("2018-08-03 07-23-14".replace(/-/g, ":"));
+console.log("2018-08-03 07-23-14".replace(/\d/g, "9"));
 
-const result1 = /java/gi.exec(str);
-console.log(result1[0]);
-console.log(result1.index);
-console.log(result1.input);
+const littleWomen = "Meg March, Jo March, Beth March, Amy March";
+console.log(littleWomen.replace(/\w+ March/gi, "Mrs.$&"));
+console.log(
+  littleWomen.replace(/\w+ March/gi, (str, d1, d2, d3, d4, offset, s) => {
+    let tag = "";
+    if (/Meg/.test(str)) tag = "첫째";
+    else if (/Jo/.test(str)) tag = "둘째";
+    else if (/Beth/.test(str)) tag = "셋째";
+    else if (/Amy/.test(str)) tag = "넷째";
 
-const nums = '"1", "2", "3"';
-const regex = /\d+/g;
-while ((result2 = regex.exec(nums))) {
-  console.log(result2, regex.lastIndex);
-}
+    console.log(`${str}은 ${tag}`);
+    return tag;
+  })
+);
+
+const name = "March Amy";
+console.log(name.replace(/(March) (Amy)/, "$2 $1"));
+console.log(
+  name.replace(/(March) (Amy)/, (str, first, second, offet, s) => {
+    console.log(`${second} is name ${first} is first name`);
+    return `${second} ${first}`;
+  })
+);
