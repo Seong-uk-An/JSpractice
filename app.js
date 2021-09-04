0e56f840d51fefa2e12f54fc613a4452b67a973b
Seong-uk-An <asu970602@naver.com>
2021-09-04 13:28:50 +0900

    test

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const str = "To lose your path is the way to find that path";
+const numRegExp = /[0-9]+/;
+const phoneRegExp = /\d{3}-\d{3,4}-\d{4}$/;
+const emailRegExp = /^([-_.]?[0-9a-zA-Z]{6,13})+\@([0-9a-z]+)\.([a-z]{2,3})$/i;
 
-const sensitiveCaseRegex = /to/;
-const ignoreAllCaseRegex = /to/gi;
-const findRangeRegex = /([a-f])\w+/;
-const findAllRangeRegex = /([a-f])\w+/g;
-
-console.log(str.match(sensitiveCaseRegex));
-console.log(str.match(ignoreAllCaseRegex));
-console.log(str.match(findRangeRegex));
-console.log(str.match(findAllRangeRegex));
+console.log(numRegExp.test(12345));
+console.log(numRegExp.test("test"));
+console.log(phoneRegExp.test("010-3003-0046"));
+console.log(phoneRegExp.test("02-8844-1234"));
+console.log(emailRegExp.test("test123@javascript.org"));
+console.log(emailRegExp.test("test-javascript"));
