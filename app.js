6e7c614c97ea012cc26adeb56061477fdc704eb3
Seong-uk-An <asu970602@naver.com>
2021-09-05 17:31:37 +0900

    array

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const array = [1, 2, 3, 4, 5];
 
-const first = array[0];
-const second = array[1];
-const third = array[2];
-const fourth = array[3];
-const fifth = array[4];
-console.log(first, second, third, fourth, fifth); // 1,2,3,4,5
+const fir = array[0];
+const sec = array[1];
+const thi = array[2];
+const fou = array[3];
+const fif = array[4];
+console.log(fir, sec, thi, fou, fif); // 1,2,3,4,5
 
 const [f, s, t, fo, fi] = array;
 console.log(f, s, t, fo, fi); // 1,2,3,4,5
